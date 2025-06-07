8c53cce456ed4f53f8246dba2d6c3d8f058aa196
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2025-06-07 16:09:39 +0530

    chore: update main.rs to replace is_even function call with fib function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 fn main() {
-    println!("{}", is_even(20));
+    // println!("{}", is_even(20));
+    println!("{}", fib(4));
 }
 
-fn is_even(num: i32) -> bool {
-    // Bitwise operation
-    // & 1 is faster than doing % 2
-    return num & 1 == 0;
+// fn is_even(num: i32) -> bool {
+//     // Bitwise operation
+//     // & 1 is faster than doing % 2
+//     return num & 1 == 0;
+// }
+
+fn fib(num: i32) -> i32 {
+    let mut first: i32 = 0;
+    let mut second: i32 = 1;
+
+    if num == 0 {
+        return first;
+    }
+
+    if num == 1 {
+        return second;
+    }
+
+    // Here num - 1 or num - 2 ain't needed
+    for _ in 1..num {
+        let temp = second;
+        second += first;
+        first = temp;
+    }
+
+    return second;
 }
