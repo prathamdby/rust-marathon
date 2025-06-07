8b56d8bc830b281aa8e8e089b90c3f8ca9689400
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2025-06-07 16:16:33 +0530

    chore: update main.rs to comment out fib function and add get_string_len function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     // println!("{}", is_even(20));
-    println!("{}", fib(4));
+    // println!("{}", fib(4));
+
+    let my_str = String::from("Hello, world!");
+    let length = get_string_len(my_str);
+    println!("{}", length);
 }
 
 // fn is_even(num: i32) -> bool {
@@ -9,24 +13,29 @@ fn main() {
 //     return num & 1 == 0;
 // }
 
-fn fib(num: i32) -> i32 {
-    let mut first: i32 = 0;
-    let mut second: i32 = 1;
+// fn fib(num: i32) -> i32 {
+//     let mut first: i32 = 0;
+//     let mut second: i32 = 1;
 
-    if num == 0 {
-        return first;
-    }
+//     if num == 0 {
+//         return first;
+//     }
 
-    if num == 1 {
-        return second;
-    }
+//     if num == 1 {
+//         return second;
+//     }
 
-    // Here num - 1 or num - 2 ain't needed
-    for _ in 1..num {
-        let temp = second;
-        second += first;
-        first = temp;
-    }
+//     // Here num - 1 or num - 2 ain't needed
+//     for _ in 1..num {
+//         let temp = second;
+//         second += first;
+//         first = temp;
+//     }
 
-    return second;
+//     return second;
+// }
+
+fn get_string_len(str: String) -> usize {
+    // Implicit return here, no need to put "return" or ";"
+    str.chars().count()
 }
