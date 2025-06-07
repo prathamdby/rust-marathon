854963168713b2e2b4b9afbfb50a918e56ef6130
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2025-06-07 15:40:02 +0530

    feat: implement is_even function and update main to use it
    Add an is_even function that checks if a number is even using a bitwise operation. Update the main function to print the result of is_even for the number 20.

--- rust-part-2-1/src/main.rs
+++ rust-part-2-1/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_even(20));
+}
+
+fn is_even(num: i32) -> bool {
+    // Bitwise operation
+    // & 1 is faster than doing % 2
+    return num & 1 == 0;
 }
